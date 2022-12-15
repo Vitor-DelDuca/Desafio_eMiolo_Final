e516c53b143199fb86bf262fac276aacb078e86f
Vítor Del' Duca <vitordelduca@hotmail.com>
2022-12-14 21:54:12 -0300

    Feat: Definindo rotas do projeto.

--- src/Router/index.js
+++ src/Router/index.js
@@ -1,8 +1,10 @@
-// ./src/router.js
-
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../Views/Home.vue'
-import About from '../Views/About.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import Home from '../views/Home.vue';
+import About from '../views/About.vue';
+import Login from '../views/Login.vue';
+import Register from '../views/Register.vue';
+import Feed from '../views/Feed.vue';
 
 export const routerHistory = createWebHistory()
 export const router = createRouter({
@@ -17,7 +19,46 @@ export const router = createRouter({
       path: '/about',
       component: About
     },
-  ]
-})
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/register',
+      component: Register
+    },
+    {
+        path: '/feed',
+        component: Feed,
+        meta: { requiresAuth: true }
+    }
+  ],
+});
+
+const getCurrentUser = () => {
+    return new Promise((resolve, reject) => {
+        const removeListener = onAuthStateChanged(
+            getAuth(),
+            (user) => {
+                removeListener();
+                resolve(user);
+            },
+            reject
+        );
+    });
+};
+
+router.beforeEach(async (to, from, next) => {
+    if (to.matched.some((record) => record.meta.requiresAuth)) {
+        if (await getCurrentUser()) {
+            next();
+        } else {
+            alert("Você não tem acesso!");
+            next("/");
+        }
+    } else {
+        next();
+    }
+});
 
 export default router;
